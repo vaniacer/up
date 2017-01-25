8b6778e39c957afc70dd0b4ca988730d0038597f
Vaniac <ie.marov@gmail.com>
2017-01-25 14:53:58 +0300

    add groups

--- ups/models.py
+++ ups/models.py
@@ -1,19 +1,18 @@
 # -*- encoding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 
 class Project(models.Model):
 	"""Проект: web-исполнение, web-нси, web-соглашения"""
-	name  = models.CharField(max_length=200)
-	desc  = models.TextField()
-	date  = models.DateTimeField(auto_now_add=True)
-	owner = models.ForeignKey(User)
-	users     = [ models.ForeignKey(User), ]
-	dumpers   = [ models.ForeignKey(User), ]
-	updaters  = [ models.ForeignKey(User), ]
-	uploaders = [ models.ForeignKey(User), ]
+	name = models.CharField(max_length=200)
+	desc = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+	view = models.ForeignKey(Group, related_name='view')
+	dump = models.ForeignKey(Group, related_name='dump')
+	updt = models.ForeignKey(Group, related_name='updt')
+	upld = models.ForeignKey(Group, related_name='upld')
 
 	def __unicode__(self):
 		"""Возвращает строковое представление модели."""
