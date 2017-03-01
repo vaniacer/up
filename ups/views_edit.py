6b47e7549a61c56cfd5675e906eb736a46bfcd78
Vaniac <ie.marov@gmail.com>
2017-03-01 10:37:42 +0300

    split views in 3 part

---
+++ ups/views_edit.py
@@ -0,0 +1,72 @@
+# -*- encoding: utf-8 -*-
+
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm, ServerForm, UpdateForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from .models import Project, Server, Update
+from .groups import *
+
+
+@login_required
+def edit_project(request, project_id):
+	"""Редактирует существующий проект."""
+	project = Project.objects.get(id=project_id)
+
+	if request.method != 'POST':
+		# Исходный запрос; форма заполняется данными текущей записи.
+		check_groups(project)
+		form = ProjectForm(instance=project)
+	else:
+		# Отправка данных POST; обработать данные.
+		form = ProjectForm(instance=project, data=request.POST)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('ups:project', args=[project.id]))
+
+	context = {'project': project, 'form': form}
+	return render(request, 'ups/edit_project.html', context)
+
+
+@login_required
+def edit_server(request, server_id):
+	"""Редактирует существующий сервер."""
+	server = Server.objects.get(id=server_id)
+	project = server.project
+
+	if request.method != 'POST':
+		# Исходный запрос; форма заполняется данными текущей записи.
+		form = ServerForm(instance=server)
+	else:
+		# Отправка данных POST; обработать данные.
+		form = ServerForm(instance=server, data=request.POST)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('ups:project', args=[project.id]))
+
+	context = {'server': server, 'project': project, 'form': form}
+	return render(request, 'ups/edit_server.html', context)
+
+
+@login_required
+def edit_update(request, update_id):
+	"""Редактирует существующий пакет обновлений."""
+	update = Update.objects.get(id=update_id)
+	project = update.project
+
+	if request.method != 'POST':
+		# Исходный запрос; форма заполняется данными текущей записи.
+		form = UpdateForm(instance=update)
+	else:
+		# Отправка данных POST; обработать данные.
+		form = UpdateForm(request.POST, request.FILES, instance=update)
+
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('ups:project', args=[project.id]))
+
+	context = {'update': update, 'project': project, 'form': form}
+	return render(request, 'ups/edit_update.html', context)
---
+++ ups/views_new.py
@@ -0,0 +1,73 @@
+# -*- encoding: utf-8 -*-
+
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm, ServerForm, UpdateForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from .models import Project
+from .groups import *
+
+
+@login_required
+def new_project(request):
+	"""Определяет новый проект."""
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		create_dummy()
+		form = ProjectForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = ProjectForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			create_groups(request.POST['name'])
+			return HttpResponseRedirect(reverse('ups:projects'))
+
+	context = {'form': form}
+	return render(request, 'ups/new_project.html', context)
+
+
+@login_required
+def new_server(request, project_id):
+	"""Добавляет новый сервер."""
+	project = Project.objects.get(id=project_id)
+
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		form = ServerForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = ServerForm(data=request.POST)
+
+		if form.is_valid():
+			server = form.save(commit=False)
+			server.project = project
+			server.save()
+			return HttpResponseRedirect(reverse('ups:project', args=[project_id]))
+
+	context = {'project': project, 'form': form}
+	return render(request, 'ups/new_server.html', context)
+
+
+@login_required
+def new_update(request, project_id):
+	"""Добавляет новое обновление."""
+	project = Project.objects.get(id=project_id)
+
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		form = UpdateForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = UpdateForm(request.POST, request.FILES)
+
+		if form.is_valid():
+			update = form.save(commit=False)
+			update.project = project
+			update.save()
+			return HttpResponseRedirect(reverse('ups:project', args=[project_id]))
+
+	context = {'project': project, 'form': form}
+	return render(request, 'ups/new_update.html', context)
