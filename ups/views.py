70b587fe0bc796024a9c64f35b9678e2940a2cdc
Vaniac <ie.marov@gmail.com>
2017-03-03 16:26:07 +0300

    check selected checkboxes

--- ups/views.py
+++ ups/views.py
@@ -28,7 +28,8 @@ def project(request, project_id):
 	selected_updates = request.POST.getlist('selected_updates')
 	selected_servers = request.POST.getlist('selected_servers')
 
-	print selected_updates, selected_servers
+	if request.POST.get('select_test'):
+		print selected_updates, selected_servers
 
 	context = {'project': project, 'servers': servers, 'updates': updates}
 	return render(request, 'ups/project.html', context)
