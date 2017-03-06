7278b13fd65f2a27979c8deeaada1dc31abe6d05
Vaniac <ie.marov@gmail.com>
2017-03-06 17:01:46 +0300

    send checked updates and servers to bash

--- ups/views.py
+++ ups/views.py
@@ -1,9 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from .models import Project, Server, Update
 from django.shortcuts import render
-from .models import Project
-
+from subprocess import call
 
 def index(request):
 	"""Домашняя страница приложения update server"""
@@ -20,7 +21,7 @@ def projects(request):
 
 @login_required
 def project(request, project_id):
-	"""Выводит один проект, все его серверы и пакеты обновлений."""
+	"""Выводит один проект, все его серверы и пакеты обновлений, кнопки действий."""
 	project = Project.objects.get(id=project_id)
 	servers = project.server_set.order_by('name')
 	updates = project.update_set.order_by('desc')
@@ -28,8 +29,25 @@ def project(request, project_id):
 	selected_updates = request.POST.getlist('selected_updates')
 	selected_servers = request.POST.getlist('selected_servers')
 
+	SU = []
+	SS = []
+
 	if request.POST.get('select_test'):
-		print selected_updates, selected_servers
+		for id in selected_updates:
+			update = Update.objects.get(id=id)
+			SU.append(str(update))
+		print SU
+
+		for id in selected_servers:
+			server = Server.objects.get(id=id)
+			SS.append(server.addr)
+		print SS
+
+		opt = ' '.join(SS) + ' ' + ' '.join(SU)
+		run = call("up/test {}" .format(opt), shell=True)
+		print run
+
+		return HttpResponseRedirect('')
 
 	context = {'project': project, 'servers': servers, 'updates': updates}
 	return render(request, 'ups/project.html', context)
