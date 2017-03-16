c1e4ca760f4c46db46535ae78a9b268d06f0cfe8
Vaniac <ie.marov@gmail.com>
2017-03-16 17:28:16 +0300

    ref

--- ups/buttons.py
+++ ups/buttons.py
@@ -1,7 +1,5 @@
 # -*- encoding: utf-8 -*-
 
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from .models import Server, Update
 from subprocess import call
 
@@ -45,20 +43,3 @@ def select_upload(selected_updates, selected_servers):
 		opt = str(s.addr) + ' ' + str(s.wdir) + ' ' + ' '.join('media/' + str(u.file) for u in updates)
 		run = call("bash/copy {}".format(opt), shell=True)
 		print run
-
-
-def buttons(request):
-	"""Обрабатывает кнопки."""
-
-	# Selected objects ID's.
-	selected_updates = request.POST.getlist('selected_updates')
-	selected_servers = request.POST.getlist('selected_servers')
-
-	if request.POST.get('select_test'):
-		select_test(selected_updates, selected_servers)
-
-	elif request.POST.get('select_upload'):
-		select_upload(selected_updates, selected_servers)
-
-	return HttpResponseRedirect('')
-
--- ups/views.py
+++ ups/views.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from .permissions import check_perm, check_perm_button
-from .buttons import buttons
+from .buttons import select_test, select_upload
+from django.http import HttpResponseRedirect
 from .models import Project
+from django.shortcuts import render
+from .permissions import check_perm
 
 
 def index(request):
@@ -30,8 +31,18 @@ def project(request, project_id):
 	servers = current_project.server_set.order_by('name')
 	updates = current_project.update_set.order_by('date').reverse()
 
-	if check_perm_button('run_command', current_project, request.user):
-		buttons(request)
+	selected_updates = request.POST.getlist('selected_updates')
+	selected_servers = request.POST.getlist('selected_servers')
+
+	if request.POST.get('select_test'):
+		check_perm('run_command', current_project, request.user)
+		select_test(selected_updates, selected_servers)
+		return HttpResponseRedirect('')
+
+	elif request.POST.get('select_upload'):
+		check_perm('run_command', current_project, request.user)
+		select_upload(selected_updates, selected_servers)
+		return HttpResponseRedirect('')
 
 	context = {'project': current_project, 'servers': servers, 'updates': updates}
 	return render(request, 'ups/project.html', context)
