ef623016073146b8a7e59f46f12e4e877d76bf38
Vaniac <ie.marov@gmail.com>
2017-03-23 12:18:01 +0300

    quick_start.sh

---
+++ quick_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+admin=admin
+paswd=1234qwer
+email=marov@krista.ru
+
+echo -e "Add virtual env."
+virtualenv ../env
+source ../env/bin/activate
+
+echo -e "Install requirements."
+easy_install $(cat requirements.txt)
+
+echo -e "Create db and admin user."
+./manage.py makemigrations ups
+./manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser(${email}, ${admin}, ${paswd})" | \
+    python manage.py shell
+
+echo -e "Runserver."
+./manage.py runserver
