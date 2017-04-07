66181722d44cd2183320af94eb08f0a79e537103
Vaniac <ie.marov@gmail.com>
2017-04-07 11:52:46 +0300

    set cron job without output! O_o

--- ups/views.py
+++ ups/views.py
@@ -47,9 +47,10 @@ def project(request, project_id):
 	if request.POST.get('CRON'):
 		check_perm('run_command', current_project, request.user)
 		if request.POST.get('select_copy'):
-			log, err = select_cron_copy(selected_updates, selected_servers, current_project, request.user, date, time)
-			context = {'project': current_project, 'log': log, 'err': err}
-			return render(request, 'ups/output.html', context)
+			select_cron_copy(selected_updates, selected_servers, current_project, request.user, date, time)
+			# log, err = select_cron_copy(selected_updates, selected_servers, current_project, request.user, date, time)
+			# context = {'project': current_project, 'log': log, 'err': err}
+			# return render(request, 'ups/output.html', context)
 
 	if request.POST.get('RUN'):
 		check_perm('run_command', current_project, request.user)
