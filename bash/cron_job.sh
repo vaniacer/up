aab69702a3864c9ba545821dcf8dbc602dc3f1ab
Vaniac <ie.marov@gmail.com>
2017-04-28 17:00:19 +0300

    ref

--- bash/cron_job.sh
+++ bash/cron_job.sh
@@ -2,20 +2,20 @@
 
 # Get options and functions
 . $(dirname $0)/func.sh
+cronfile=/var/spool/cron/crontabs/${USER}
 
 # Get time
-read date time <<< "${date}"
-hh=${time%:*}; mm=${time#*:}; DD=${date%%.*}; MM=${date#*.}; MM=${MM%.*}
+time=${date#* }; date=${date% *}; hh=${time%:*}; mm=${time#*:}; DD=${date%%.*}; MM=${date#*.}; MM=${MM%.*}
 
-date="${mm} ${hh} ${DD} ${MM}"                              # Cron format date
-sedr="/${id}/d"                                             # Sed rule to delete old cron job
-cncl="sed \"${sedr}\" -i /var/spool/cron/crontabs/${USER}"  # Command to cancel executed cron job
-cmnd="${workdir}/${cmd} -cron ${id}"                         # Command to run
-[ "${updates}" ] && cmnd="${cmnd} -u \"${updates}\""        # Add updates to command if exist
-[ "${servers}" ] && cmnd="${cmnd} -s \"${servers}\""        # Add servers to command if exist
+sedr="/${id}/d"                                         # Sed rule to delete old cron job
+date="${mm} ${hh} ${DD} ${MM}"                          # Cron format date
+cmnd="${workdir}/${cmd} -cron ${id}"                    # Command to run
+cncl="sed \"${sedr}\" -i ${cronfile}"                   # Command to cancel executed cron job
+[ "${updates}" ] && cmnd="${cmnd} -u \"${updates}\""    # Add updates to command if exist
+[ "${servers}" ] && cmnd="${cmnd} -s \"${servers}\""    # Add servers to command if exist
 
 # Set crontab job
-(crontab -l ; echo -e "${date} * ${cmnd}; ${cncl}") | crontab -
+echo -e "${date} * ${cmnd}; ${cncl}" >> ${cronfile}
 
 # Info
 ${workdir}/${cmd} -u "${updates}" -s "${servers}" -desc true
