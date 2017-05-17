613a44180e981f40ef6dfdf8b772b98a33692bd3
Vaniac <ie.marov@gmail.com>
2017-05-17 15:32:06 +0300

    reg

--- ups/commands.py
+++ ups/commands.py
@@ -8,29 +8,29 @@ import datetime
 
 def command(selected):
 	"""Определяет команду(bash script) по полученному command id."""
-# -----------------------+-------------------+-----------------------------+-----------------+----------------
-#     -------------------|  Write history    |  Bash script names          |  Html tags in   |------------
-	dick = {  # ---------|  log or not       |                             |  output         |--------
-		# RUN only |-----+-------------------+-----------------------------+-----------------+---
+# ------------------------+------------------+-----------------------------+----------------+----------------
+#     --------------------|  Write history   |  Bash script names          |  Html tags in  |------------
+	dick = {  # ----------|  log or not      |                             |  output        |--------
+		# RUN only |------+------------------+-----------------------------+----------------+---
 		'delete_job':      {'history':  True, 'bash':      'delete_job.sh', 'tag': False, },
 		'check_logs':      {'history': False, 'bash':      'check_logs.sh', 'tag': False, },
 		'check_conf':      {'history': False, 'bash':      'check_conf.sh', 'tag': False, },
 		'check_updates':   {'history': False, 'bash':   'check_updates.sh', 'tag': False, },
-		# Maintenance |----------------------+-----------------------------+-----------------+
+		# Maintenance |----------------------+-----------------------------+----------------+
 		'stop':            {'history':  True, 'bash':            'stop.sh', 'tag': False, },
 		'start':           {'history':  True, 'bash':           'start.sh', 'tag': False, },
 		'reload':          {'history':  True, 'bash':          'reload.sh', 'tag': False, },
 		'restart':         {'history':  True, 'bash':         'restart.sh', 'tag': False, },
 		'maintenance_on':  {'history':  True, 'bash':  'maintenance_on.sh', 'tag': False, },
 		'maintenance_off': {'history':  True, 'bash': 'maintenance_off.sh', 'tag': False, },
-		# Delete updates |-------------------+-----------------------------+-----------------+
+		# Delete updates |-------------------+-----------------------------+----------------+
 		'del_all_updates': {'history':  True, 'bash': 'del_all_updates.sh', 'tag': False, },
 		'del_sel_updates': {'history':  True, 'bash': 'del_sel_updates.sh', 'tag': False, },
-		# Backup |---------------------------+-----------------------------+-----------------+
+		# Backup |---------------------------+-----------------------------+----------------+
 		'backup_db':       {'history':  True, 'bash':       'backup_db.sh', 'tag': False, },
 		'backup_sys':      {'history':  True, 'bash':      'backup_sys.sh', 'tag': False, },
 		'backup_full':     {'history':  True, 'bash':     'backup_full.sh', 'tag': False, },
-		# Main |-----------------------------+-----------------------------+-----------------+
+		# Main |-----------------------------+-----------------------------+----------------+
 		'copy':            {'history':  True, 'bash':            'copy.sh', 'tag': False, },
 		'script':          {'history':  True, 'bash':          'script.sh', 'tag': False, },
 		'update':          {'history':  True, 'bash':          'update.sh', 'tag': False, },
