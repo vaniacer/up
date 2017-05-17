45143e1dcdac1c676f7b52e8ce790f4a772f3771
Vaniac <ie.marov@gmail.com>
2017-05-17 11:37:08 +0300

    bash rename

--- ups/commands.py
+++ ups/commands.py
@@ -12,31 +12,31 @@ def command(selected):
 # -----------------------|  Write history    |  Bash command name          |  Html tags in   |-----------------
 #     -------------------|  log or not       |                             |  output         |------------
 	dick = {  # ---------+-------------------+-----------------------------+-----------------+--------
-		# RUN only commands |---------------------------------------------------------------
+		# RUN only commands |----------------+-----------------------------+-----------------+
 		'delete_job':      {'history':  True, 'bash':      'delete_job.sh', 'tag': False, },
 		'check_logs':      {'history': False, 'bash':      'check_logs.sh', 'tag': False, },
 		'check_conf':      {'history': False, 'bash':      'check_conf.sh', 'tag': False, },
 		'check_updates':   {'history': False, 'bash':   'check_updates.sh', 'tag': False, },
-		# Maintenance |---------------------------------------------------------------------
+		# Maintenance |----------------------+-----------------------------+-----------------+
 		'stop':            {'history':  True, 'bash':            'stop.sh', 'tag': False, },
 		'start':           {'history':  True, 'bash':           'start.sh', 'tag': False, },
 		'reload':          {'history':  True, 'bash':          'reload.sh', 'tag': False, },
 		'restart':         {'history':  True, 'bash':         'restart.sh', 'tag': False, },
 		'maintenance_on':  {'history':  True, 'bash':  'maintenance_on.sh', 'tag': False, },
 		'maintenance_off': {'history':  True, 'bash': 'maintenance_off.sh', 'tag': False, },
-		# Delete updates |------------------------------------------------------------------
-		'del_all_updates': {'history':  True, 'bash':        'delallup.sh', 'tag': False, },
-		'del_sel_updates': {'history':  True, 'bash':        'delselup.sh', 'tag': False, },
-		# Backup |--------------------------------------------------------------------------
+		# Delete updates |-------------------+-----------------------------+-----------------+
+		'del_all_updates': {'history':  True, 'bash': 'del_all_updates.sh', 'tag': False, },
+		'del_sel_updates': {'history':  True, 'bash': 'del_sel_updates.sh', 'tag': False, },
+		# Backup |---------------------------+-----------------------------+-----------------+
 		'backup_db':       {'history':  True, 'bash':       'backup_db.sh', 'tag': False, },
 		'backup_sys':      {'history':  True, 'bash':      'backup_sys.sh', 'tag': False, },
 		'backup_full':     {'history':  True, 'bash':     'backup_full.sh', 'tag': False, },
-		# Main |----------------------------------------------------------------------------
+		# Main |-----------------------------+-----------------------------+-----------------+
 		'copy':            {'history':  True, 'bash':            'copy.sh', 'tag': False, },
 		'script':          {'history':  True, 'bash':          'script.sh', 'tag': False, },
 		'update':          {'history':  True, 'bash':          'update.sh', 'tag': False, },
 		'get_dump':        {'history':  True, 'bash':        'get_dump.sh', 'tag':  True, },
-		'copy_utils':      {'history':  True, 'bash':          'cutils.sh', 'tag': False, },
+		'copy_utils':      {'history':  True, 'bash':      'copy_utils.sh', 'tag': False, },
 		'sql_script':      {'history':  True, 'bash':      'sql_script.sh', 'tag': False, }, }
 
 	cmd = ''.join(selected['command'])
