397f764cca901cac8b5a971339fa6e40f4ef91d4
Vaniac <ie.marov@gmail.com>
2017-05-25 11:07:59 +0300

    download ref

--- ups/views.py
+++ ups/views.py
@@ -40,6 +40,16 @@ def pagination(request, history):
 	return history, hist_fd, hist_bk
 
 
+def download(file_path, file_name):
+	file_mimetype = mimetypes.guess_type(file_path)
+	file_wrapper = FileWrapper(file(file_path, 'rb'))
+	response = HttpResponse(file_wrapper, content_type=file_mimetype)
+	response['Content-Disposition'] = 'attachment; filename=%s' % file_name
+	response['Content-Length'] = os.stat(file_path).st_size
+	response['X-Sendfile'] = file_path
+	return response
+
+
 @login_required
 def projects(request):
 	"""Выводит список проектов."""
@@ -53,30 +63,14 @@ def download_upd(request, project_id, update_id):
 	update = get_object_or_404(Update, id=update_id)
 	current_project = get_object_or_404(Project, id=project_id)
 	check_perm('view_project', current_project, request.user)
-
-	file_path = str(update.file)
-	file_mimetype = mimetypes.guess_type(file_path)
-	file_wrapper = FileWrapper(file(file_path, 'rb'))
-	response = HttpResponse(file_wrapper, content_type=file_mimetype)
-	response['Content-Disposition'] = 'attachment; filename=%s' % str(update)
-	response['Content-Length'] = os.stat(file_path).st_size
-	response['X-Sendfile'] = file_path
-	return response
+	return download(str(update.file), str(update))
 
 
 @login_required
 def download_dump(request, project_id, dump):
 	current_project = get_object_or_404(Project, id=project_id)
 	check_perm('view_project', current_project, request.user)
-
-	file_path = conf.MEDIA_ROOT + '/updates/dumps/' + str(dump)
-	file_mimetype = mimetypes.guess_type(file_path)
-	file_wrapper = FileWrapper(file(file_path, 'rb'))
-	response = HttpResponse(file_wrapper, content_type=file_mimetype)
-	response['Content-Disposition'] = 'attachment; filename=%s' % str(dump)
-	response['Content-Length'] = os.stat(file_path).st_size
-	response['X-Sendfile'] = file_path
-	return response
+	return download(conf.MEDIA_ROOT + '/updates/dumps/' + str(dump), str(dump))
 
 
 @login_required
