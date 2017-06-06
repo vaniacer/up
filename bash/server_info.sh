288c6f3d0563afb761768834b7efecafddd6fdb4
Vaniac <ie.marov@gmail.com>
2017-06-06 11:44:12 +0300

    reg

--- bash/server_info.sh
+++ bash/server_info.sh
@@ -7,31 +7,31 @@ function description () { #---------------------| Function description |--------
 function body () { #---------------------------------| Main function |--------------------------------------------------
 
     ssh ${addr} "
-        echo -e '\n<b>Hostname:</b>' \$(hostname)
+        printf '\n<b>Hostname:</b>' \$(hostname)
 
-        echo -e '\n<b>Interfaces:</b>'
+        printf '\n<b>Interfaces:</b>'
         ip a | grep 'inet ' | sed '/127.0.0.1/d; s/inet //g; s|/.*$||g'
 
-        echo -e '\n<b>Memory:</b>'
+        printf '\n<b>Memory:</b>'
         free -h
 
-        echo -e '\n<b>CPU:</b>'
+        printf '\n<b>CPU:</b>'
         lscpu
 
-        echo -e '\n<b>Disk:</b>'
+        printf '\n<b>Disk:</b>'
         df -h; echo; df -ih; echo; lsblk
 
-        echo -e '\n<b>Software:</b>'
+        printf '\n<b>Software:</b>'
         uname -a; echo
         [ -e /usr/bin/lsb_release ] && { lsb_release -a; echo; }
         [ -e /usr/bin/java        ] && { java  -version; echo; }
         [ -e /usr/bin/psql        ] && { psql  -V      ; echo; }
         [ -e /usr/sbin/nginx      ] && { nginx -v      ; echo; }
 
-        echo -e '<b>Logged in Users:</b>'
+        printf '<b>Logged in Users:</b>'
         who
 
-        echo -e '\n<b>Processes:</b>'
+        printf '\n<b>Processes:</b>'
         top -b -n1" || error=$?
 
 } #---------------------------------------------------------------------------------------------------------------------
