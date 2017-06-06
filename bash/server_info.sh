64fa64d9e6ce313bd9bc863ec5dd1835b20b1331
Vaniac <ie.marov@gmail.com>
2017-06-06 11:52:16 +0300

    fix

--- bash/server_info.sh
+++ bash/server_info.sh
@@ -7,31 +7,32 @@ function description () { #---------------------| Function description |--------
 function body () { #---------------------------------| Main function |--------------------------------------------------
 
     ssh ${addr} "
-        printf '\n<b>Hostname:</b>' \$(hostname)
+        printf '\n<b>Hostname:</b>\n'
+        hostname
 
-        printf '\n<b>Interfaces:</b>'
-        ip a | grep 'inet ' | sed '/127.0.0.1/d; s/inet //g; s|/.*$||g'
+        printf '\n<b>Interfaces:</b>\n'
+        ip a | grep 'inet ' | sed '/127.0.0.1/d; s/.*inet //g; s|/.*$||g'
 
-        printf '\n<b>Memory:</b>'
+        printf '\n<b>Memory:</b>\n'
         free -h
 
-        printf '\n<b>CPU:</b>'
+        printf '\n<b>CPU:</b>\n'
         lscpu
 
-        printf '\n<b>Disk:</b>'
+        printf '\n<b>Disk:</b>\n'
         df -h; echo; df -ih; echo; lsblk
 
-        printf '\n<b>Software:</b>'
+        printf '\n<b>Software:</b>\n'
         uname -a; echo
         [ -e /usr/bin/lsb_release ] && { lsb_release -a; echo; }
         [ -e /usr/bin/java        ] && { java  -version; echo; }
         [ -e /usr/bin/psql        ] && { psql  -V      ; echo; }
         [ -e /usr/sbin/nginx      ] && { nginx -v      ; echo; }
 
-        printf '<b>Logged in Users:</b>'
+        printf '<b>Logged in Users:</b>\n'
         who
 
-        printf '\n<b>Processes:</b>'
+        printf '\n<b>Processes:</b>\n'
         top -b -n1" || error=$?
 
 } #---------------------------------------------------------------------------------------------------------------------
