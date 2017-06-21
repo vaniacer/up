e0e75d6b6883fab42a8f76215a635b161df0d529
Vaniac <ie.marov@gmail.com>
2017-06-21 16:44:08 +0300

    fix

--- bash/tunnel.sh
+++ bash/tunnel.sh
@@ -10,7 +10,7 @@ function body () { #---------------------------------| Main function |----------
     rport=${port}   #8080
 
     until ! netstat -ln | grep ${lport} > /dev/null; do ((lport++)); done
-    echo -e "\n<a class=\"btn btn-primary\" href=\"http://__URL__:${lport}/login\">Connect</a>\n"
+    echo -e "\n<a class=\"btn btn-primary\" href=\"http://__URL__:${lport}\">Connect</a>\n"
     ssh ${addr} -f -L 0.0.0.0:${lport}:127.0.0.1:${rport} sleep 10 || error=$?
 
 } #---------------------------------------------------------------------------------------------------------------------
