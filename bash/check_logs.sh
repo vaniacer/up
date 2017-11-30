78cbb7b0fb0b0aa8f1c48f9f0281b3b80b038358
Vaniac <ie.marov@gmail.com>
2017-11-30 17:23:50 +0300

    change logs order in check_logs.sh, server log is at the end now

--- bash/check_logs.sh
+++ bash/check_logs.sh
@@ -6,9 +6,9 @@ function description () { #---------------------| Function description |--------
 
 function body () { #---------------------------------| Main function |--------------------------------------------------
 
-    ssh $sopt $addr "cat $wdir/jboss-bas-*/standalone/log/server.log" || error=$?
     ssh $sopt $addr  "ls $wdir/jboss-bas-*/standalone/log | grep $(date +'%Y-%m-%d')" \
         && { ssh $sopt $addr "cat $wdir/jboss-bas-*/standalone/log/server.log.$(date +'%Y-%m-%d')*" || error=$?; }
+    ssh $sopt $addr "cat $wdir/jboss-bas-*/standalone/log/server.log" || error=$?
 
 } #---------------------------------------------------------------------------------------------------------------------
 
