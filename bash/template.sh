33d406b4a61b92250d62c5b850e3c4694078fa02
Vaniac <ie.marov@gmail.com>
2017-12-01 09:32:02 +0300

    change info

--- bash/template.sh
+++ bash/template.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 function description () { #---------------------| Function description |------------------------------------------------
-    # Put description here. Variables: ${servers[@]}, ${updates[@]}, ${scripts[@]}, ${jobs[@]}, $cmd. Example:
+    # Put description here. Variables: ${servers[@]}, ${updates[@]}, ${scripts[@]}, ${jobs[@]}, $cmd.
+    # This function used to make cron jobs description. To understand what this cron job will do.
+    # Example:
     printf "Copy Update(s):\n"; for i in "${updates[@]}"; { echo "$i"; }
     printf "\nto Server(s):\n"; for i in "${servers[@]}"; { echo "$i"; }
 }
