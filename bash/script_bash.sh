7d8f02793b35e86be223b3f0ccfb497865db5231
Vaniac <ie.marov@gmail.com>
2017-12-15 10:47:37 +0300

    add copy updates while run bash script if updates where selected and add they names as options to the script

--- bash/script_bash.sh
+++ bash/script_bash.sh
@@ -9,11 +9,18 @@ function body () { #---------------------------------| Main function |----------
 
     for file in "${scripts[@]}"; { filename=${file##*/}
 
+        # If updates where selected copy them too and add as options to the script
+        [[ ${updates[@]} ]] && {
+            rsync -e "ssh $sopt" --progress -lzuogthvr ${updates[@]} $addr:$wdir/updates/new && {
+                for U in ${updates[@]}; { opt+=" updates/new/${U##*/}"; }
+            } || { error=$?; echo -e "File copy error."; return $error; }
+        }
+
         # Copy script to server
         rsync -e "ssh $sopt" --progress -lzuogthvr $file $addr:$wdir/updates/new/ > /dev/null && {
 
             printf "\nRun  script - $filename\n"
-            ssh $sopt $addr "cd $wdir; chmod +x updates/new/$filename; updates/new/$filename" || error=$?
+            ssh $sopt $addr "cd $wdir; chmod +x updates/new/$filename; updates/new/$filename $opt" || error=$?
 
             # Delete script after execution
             ssh $sopt $addr "rm $wdir/updates/new/$filename" || error=$?
