03c709c95016d712deaa0d83502cd3728c9c13e0
Vaniac <ie.marov@gmail.com>
2018-06-19 17:42:26 +0300

    ref

--- bash/remote_db.sh
+++ bash/remote_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 confolder="$1"
+filename="${2:-$dbname}_`printf "%(%d-%m-%Y)T"`.gz"
 
 rawdta=$(grep '"DataaccessDS"' -A12 "$confolder"/jboss-bas-*/standalone/configuration/standalone-full.xml)
 dbuser=${rawdta//*<user-name>/}; dbuser=${dbuser//<\/user-name>*/}
@@ -9,8 +10,6 @@ dbhost=${rawdta//*:\/\//};       dbhost=${dbhost//:[0-9]*/}
 dbport=${rawdta//*${dbhost}:/};  dbport=${dbport//\/*/}
 dbname=${rawdta//*${dbport}\//}; dbname=${dbname//<*/}
 dbopts="-h $dbhost -p $dbport -U $dbuser"
-
-filename="${2:-$dbname}_`printf "%(%d-%m-%Y)T"`.gz"
 
 PGPASSWORD="$dbpass" pg_dump -Ox $dbopts -d $dbname | gzip > "$filename" && printf "$filename" \
     || {
