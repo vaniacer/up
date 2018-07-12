93d8a822768bad99b78936bf48c007dc297fd337
Vaniac <ie.marov@gmail.com>
2018-07-12 15:53:56 +0300

    copy_utils

---
+++ modules/copy_utils.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+from os.path import expanduser
+
+
+def description(args):
+	return "\nCopy utils folder to server {server}\n".format(server=args.server)
+
+
+def run(args):
+
+	home = expanduser('~')
+	message = {'top': '\n<b>Копирую скрипты</b>\n', 'bot': ''}
+	command = [
+		'rsync', '-e', 'ssh', '--progress', '-lzuogthvr', '{home}/utils'.format(home=home),
+		'{addr}:~/.utils'.format(addr=args.server),
+	]
+	dick = {'command': command, 'message': message, 'download': ''}
+
+	return dick
