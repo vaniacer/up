33878eb95498ad36dd0c58869e421d6a64357938
Vaniac <ie.marov@gmail.com>
2018-07-19 12:41:22 +0300

    fix log size

--- dbwriter.py
+++ dbwriter.py
@@ -15,14 +15,13 @@ except IndexError:
 
 log_filename = LOG_FILE + key
 log_body = open(log_filename, 'r').read()
-num_lines = str.count(log_body, '\n')
+log_size = len(log_body)
 
-if num_lines > 100:
-	splited = log_body.split('\n')
+if log_size > 2000:
 	log_body = '{head!s}{first!s}\n...\n{last!s}'.format(
 		head='<b>Log is too long to store in history, cutting</b>\n',
-		first='\n'.join(splited[:50]),
-		last='\n'.join(splited[-50:]),
+		first=log_body[:1000],
+		last=log_body[-1000:],
 	)
 
 types = {
@@ -39,21 +38,3 @@ update = 'UPDATE {tab} SET "desc" = $$ {log} $${ext} WHERE {col};'.format(
 
 opt = ['psql', '-U', dbuser, '-h', dbhost, '-p', dbport, '-d', dbname, '-c', update]
 Popen(opt, env={"PGPASSWORD": dbpass})
-
-# Variant with psycopg2, but my is slightly faster
-# import psycopg2
-# # Connect to an existing database
-# conn = psycopg2.connect(host=dbhost, port=dbport, dbname=dbname, user=dbuser, password=dbpass)
-# cur = conn.cursor()  # Open a cursor to perform database operations
-#
-# # Execute a command: update
-# cur.execute(u'UPDATE {tab} SET "desc" = $$ {log} $${ext} WHERE {col};'.format(
-# 	col=types[typ]['col'],
-# 	tab=types[typ]['tab'],
-# 	ext=types[typ]['ext'],
-# 	log=log_body,
-# ))
-#
-# conn.commit()  # Make the changes to the database persistent
-# cur.close()    # Close communication
-# conn.close()   # with the database
