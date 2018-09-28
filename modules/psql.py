991a3edfdec4ea333181f1909a6c11cdbff4736e
Vaniac <ie.marov@gmail.com>
2018-09-28 10:01:44 +0300

    ref move all sql trash from starter to psql

--- modules/psql.py
+++ modules/psql.py
@@ -1,10 +1,14 @@
 # -*- encoding: utf-8 -*-
 
+from modules.uniq import uniq
+from datetime import datetime
 from subprocess import call, check_output
 from conf import dbname, dbhost, dbpass, dbport, dbuser
 
 
 def psql(query, select=False):
+	"""Клиент посгреса."""
+
 	psql_opt = ['psql', '-U', dbuser, '-h', dbhost, '-p', dbport, '-d', dbname, '-tAc', query]
 	if select:
 		data = check_output(psql_opt, env={"PGPASSWORD": dbpass})
@@ -12,3 +16,48 @@ def psql(query, select=False):
 	else:
 		error = call(psql_opt, env={"PGPASSWORD": dbpass})
 		return error
+
+
+def cron_log(args, error, log):
+	"""Создает лог после выполнения задания в кроне."""
+
+	today = datetime.today()
+	date = today.strftime('%Y-%m-%d %H:%M')
+	data = psql("SELECT perm, name, proj_id, user_id, serv_id FROM ups_job WHERE cron='{}';".format(args.key), True)
+	perm, name, proj_id, user_id, serv_id = data.split('|')
+
+	sql = u'''
+		INSERT INTO ups_history VALUES
+		(DEFAULT, current_timestamp, '{name}', '{cron}', '{cdat}', {exit}, '', {proj}, {user}, '{uniq}', {serv});
+		UPDATE ups_history SET \"desc\" = $$ {desc} $$ WHERE uniq='{uniq}';
+	'''.format(
+		cron=args.key,
+		proj=proj_id,
+		serv=serv_id,
+		user=user_id,
+		uniq=uniq(),
+		exit=error,
+		name=name,
+		cdat=date,
+		desc=log,)
+
+	if perm == 'f':
+		sql += u"DELETE FROM ups_job WHERE cron='{}';".format(args.key)
+
+	return psql(sql)
+
+
+def regular_log(args, error, log):
+	"""Логирует все остальные комманды."""
+
+	sql = u"UPDATE ups_history SET \"desc\" = $$ {desc} $$, exit={exit} WHERE uniq='{uniq}';".format(
+		uniq=args.key,
+		exit=error,
+		desc=log,)
+
+	if args.cron:
+		sql += u"UPDATE ups_job SET \"desc\" = $$ {desc} $$ WHERE cron='{cron}';".format(
+			cron=args.key,
+			desc=log,)
+
+	return psql(sql)
--- starter.py
+++ starter.py
@@ -5,12 +5,11 @@ from os import remove
 from time import sleep
 from getpass import getuser
 from datetime import datetime
-from modules.psql import psql
-from modules.uniq import uniq
 from os.path import join as opj
 from importlib import import_module
 from argparse import ArgumentParser
 from modules.log_cutter import log_cutter
+from modules.psql import cron_log, regular_log
 from up.settings import LOG_FILE, ERR_FILE, BASE_DIR
 
 
@@ -84,51 +83,15 @@ with open(logfile) as f:
 log = log_cutter(fullog)
 
 if args.from_cron:
-	today = datetime.today()
-	date = today.strftime('%Y-%m-%d %H:%M')
-	data = psql("SELECT perm, name, proj_id, user_id, serv_id FROM ups_job WHERE cron='{}';".format(args.key), True)
-	perm, name, proj_id, user_id, serv_id = data.split('|')
-
-	sql = u'''
-		INSERT INTO ups_history VALUES
-		(DEFAULT, current_timestamp, '{name}', '{cron}', '{cdat}', {exit}, '', {proj}, {user}, '{uniq}', {serv});
-		UPDATE ups_history SET \"desc\" = $$ {desc} $$ WHERE uniq='{uniq}';
-	'''.format(
-		cron=args.key,
-		proj=proj_id,
-		serv=serv_id,
-		user=user_id,
-		uniq=uniq(),
-		exit=error,
-		name=name,
-		cdat=date,
-		desc=log,
-	)
-	if perm == 'f':
-		sql += u"DELETE FROM ups_job WHERE cron='{}';".format(args.key)
-
-	psql(sql)
-
+	error += cron_log(args, error, log)
 else:
 	if args.history:
-
-		sql = u"UPDATE ups_history SET \"desc\" = $$ {desc} $$, exit={exit} WHERE uniq='{uniq}';".format(
-			uniq=args.key,
-			exit=error,
-			desc=log,
-		)
-
-		if args.cron:
-			sql += u"UPDATE ups_job SET \"desc\" = $$ {desc} $$ WHERE cron='{cron}';".format(
-				cron=args.key,
-				desc=log,
-			)
-
-		error += psql(sql)
+		error += regular_log(args, error, log)
 
 	with open(errfile, 'w') as f:
 		f.write(str(error))
 
+# Delete log files }-------------------
 sleep(10)
 for f in logfile, errfile:
 	try:
