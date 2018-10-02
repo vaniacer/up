6960de1ff6d5aa816ffc9e2d30df4a92a4bcc475
Vaniac <ie.marov@gmail.com>
2018-10-02 16:14:59 +0300

    ref logcuter

--- modules/log_cutter.py
+++ modules/log_cutter.py
@@ -1,15 +1,19 @@
 # -*- encoding: utf-8 -*-
 
+from conf import logsize
+
+halfsize = logsize / 2
+
 
 def log_cutter(log):
 
 	log = log.decode('utf-8', errors='replace')
 	log_size = len(log)
-	if log_size > 4000:
+	if log_size > logsize:
 		cutted_log = u'{head!s}{first!s}\n...\n{last!s}'.format(
 			head='<b>Log is too long to store in history, cutting</b>\n',
-			first=log[:2000],
-			last=log[-2000:],
+			first=log[:halfsize],
+			last=log[-halfsize:],
 		)
 
 		return cutted_log
