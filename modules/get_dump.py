30588472251ed7b23ac072731f8898f34c24b25e
Vaniac <ie.marov@gmail.com>
2018-11-19 13:14:38 +0300

    ref

--- modules/get_dump.py
+++ modules/get_dump.py
@@ -43,9 +43,7 @@ def run(args, log):
 
 	error = my_call(command, log)
 	if error == 0:
-		download_error = download_file(download, args.server, log)
-		if download_error > 0:
-			error = download_error
+		error += download_file(download, args.server, log)
 
 		message(
 			"\n<a class='btn btn-primary' href='/download_dump/{pro}/{file}'>Download</a>".format(
