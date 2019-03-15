cd30d99c14fa761518f7d9d4f4d56c0c81156328
Vaniac <ie.marov@gmail.com>
2019-03-15 15:54:02 +0300

    remove spaces

--- modules/get_dump.py
+++ modules/get_dump.py
@@ -51,9 +51,9 @@ def run(args, log):
 				<div class="input-group col-md-10">
 					<div class="input-group-btn">
 						<input class="form-control" type="text"
-						value="curl {OP} https://ups.krista.ru/dumps/{PN}/{FN} | zcat | psql testdb" id="DBC">					
+						value="curl {OP} https://ups.krista.ru/dumps/{PN}/{FN} | zcat | psql testdb" id="DBC">
 						<input onclick="copy_to_clipboard('DBC')" type="button" value="Copy" class="btn btn-primary"
-						title="Copy to clipboard"/>				
+						title="Copy to clipboard"/>
 					</div>
 				</div>
 			""".format(
