1bc9756d64dceb8cfeb1fb184123b335ebdbf519
Vaniac <ie.marov@gmail.com>
2019-08-09 16:06:00 +0300

    ref xml_parser

--- modules/xml_parser.py
+++ modules/xml_parser.py
@@ -5,34 +5,28 @@ import xml.etree.cElementTree as ET
 
 
 def parser(xml):
-	tree = ET.fromstring(xml)
-	iter_ = tree.getiterator()
-	connection = ''
-	username = ''
-	password = ''
+	root = ET.fromstring(xml)
+	tag = root.tag.replace('}server', '}')
+	pro = root.find(tag + 'profile')
+	for child in pro:
+		if 'datasources' in child.tag:
+			data_tag = child.tag.replace('}subsystem', '}')
+			sub_data = child
+			break
 
-	for elem in iter_:
-		name = elem.get('pool-name')
-		if name == 'DataaccessDS':
-			prefix = elem.tag.replace('}datasource', '}')
-			children = elem.getchildren()
-			for child in children:
-				if child.tag == '{}connection-url'.format(prefix):
-					connection = child.text
-				if child.tag == '{}security'.format(prefix):
-					sec_children = child.getchildren()
-					for sec_child in sec_children:
-						if sec_child.tag == '{}user-name'.format(prefix):
-							username = sec_child.text
-						if sec_child.tag == '{}password'.format(prefix):
-							password = sec_child.text
+	for child in sub_data[0]:
+		if child.attrib:
+			if child.attrib['pool-name'] == 'DataaccessDS':
+				connection = child.find(data_tag + 'connection-url').text
+				security = child.find(data_tag + 'security')
+				dbuser = security.find(data_tag + 'user-name').text
+				dbpass = security.find(data_tag + 'password').text
+				break
 
 	splitted_connection = connection.split('/')
 	dbport = splitted_connection[-2].split(':')[-1]
 	dbhost = splitted_connection[-2].split(':')[0]
 	dbname = splitted_connection[-1]
-	dbuser = username
-	dbpass = password
 
 	return dbhost, dbport, dbname, dbuser, dbpass
 
