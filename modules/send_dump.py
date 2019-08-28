d78057a89c80279bdacb5d1a08b982108b8f64fe
Vaniac <ie.marov@gmail.com>
2019-08-28 14:40:08 +0300

    remove messages from sendump

--- modules/send_dump.py
+++ modules/send_dump.py
@@ -43,17 +43,11 @@ def run(args, log):
 			dbconn="ALTER DATABASE $dbname ALLOW_CONNECTIONS false;"
 			dbterm="SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$dbname';"
 
-			printf "\n$dbterm\n"
-			printf "\nRestrict DB connections"
 			psql     $dbopts -c "$dbconn" &> /dev/null || ((error+=$?))
-			printf "\nTerminate DB connections"
 			psql     $dbopts -c "$dbterm"              || ((error+=$?))
-			printf "\nDrop DB"
 			dropdb   $dbopts     $dbname               || ((error+=$?))
-			printf "\nRecreate DB"
 			createdb $dbopts -O  $dbuser     $dbname   || ((error+=$?))
 
-			printf "\nRestore dump"
 			gunzip -c {tmp}/{file} | psql -v ON_ERROR_STOP=1 $dbopts -d $dbname
 			for i in ${{PIPESTATUS[@]}}; {{ ((error+=$i)); }}
 			exit $error
