404a33548f8681d3f8732f2cb45fc9ba1dbc3d20
Vaniac <ie.marov@gmail.com>
2019-11-29 17:47:03 +0300

    ref

--- users/views.py
+++ users/views.py
@@ -42,15 +42,15 @@ def register(request):
 			user.is_active = False
 			user.save()
 			current_site = get_current_site(request)
-			mail_subject = 'UpS account activation'
+			subject = 'UpS account activation'
 			message = render_to_string('users/activation_email.html', {
 				'user': user,
 				'domain': current_site.domain,
 				'uid': urlsafe_base64_encode(force_bytes(user.pk)),
 				'token': account_activation_token.make_token(user),
 			})
-			to_email = form.cleaned_data.get('email')
-			email = EmailMessage(mail_subject, message, to=[to_email])
+			toadr = form.cleaned_data.get('email')
+			email = EmailMessage(subject, message, to=[toadr])
 			email.send()
 			context = {'mess': 'Please confirm your email address to complete the registration.'}
 			return render(request, 'users/after_register.html', context)
