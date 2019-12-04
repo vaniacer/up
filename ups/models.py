79025d4856a5f2e1f95c876e6cb67ca0cd57ebd0
Vaniac <ie.marov@gmail.com>
2019-12-04 18:13:05 +0300

    ref

--- ups/models.py
+++ ups/models.py
@@ -220,7 +220,7 @@ class Profile(models.Model):
 	script = models.BooleanField(default=False)  # Show only my scripts
 	update = models.BooleanField(default=False)  # Show only my updates
 	server = models.BooleanField(default=False)  # Show only my servers
-	cron_fltr = models.CharField(max_length=255, default='',   blank=True)  # Default cron jobs filter
+	cron_fltr = models.CharField(max_length=255,   default='', blank=True)  # Default cron jobs filter
 	script_fltr = models.CharField(max_length=255, default='', blank=True)  # Default scripts filter
 	update_fltr = models.CharField(max_length=255, default='', blank=True)  # Default updates filter
 	server_fltr = models.CharField(max_length=255, default='', blank=True)  # Default servers filter
