e5ff9b27eadc51009cdf147e3130ad37064dd946
Vaniac <ie.marov@gmail.com>
2020-01-14 09:06:00 +0300

    add cat *.failed if jboss.start failed

--- modules/start.py
+++ modules/start.py
@@ -12,4 +12,14 @@ def run(args, log):
 	message('\n<b>Выполняю jboss.start</b>\n'.format(wdir=args.wdir), log)
 	command = ['ssh', args.server, '{wdir}/krupd jboss.start'.format(wdir=args.wdir)]
 	error = my_call(command, log)
+	if error:
+		command = [
+			'ssh', args.server,
+			'''	cd {wdir}/jboss-bas-*/standalone/deployments
+				for file in *.failed; {{
+					echo "------------{{ $file }}------------"
+					cat $file			
+				}}
+			'''.format(wdir=args.wdir)]
+		error += my_call(command, log)
 	return error
